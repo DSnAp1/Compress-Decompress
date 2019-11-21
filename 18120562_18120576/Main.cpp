f2380e2d7b19daafafa5ddb43a722aa2d89d52ba
DangThanh2010 <57797011+DangThanh2010@users.noreply.github.com>
2019-11-21 19:35:48 +0700

    Update Main.cpp
    xóa phần test

--- 18120562_18120576/Main.cpp
+++ 18120562_18120576/Main.cpp
@@ -3,14 +3,8 @@
 int main()
 
 {
-	
-	
 	CompressStringFile("in.txt","out.th");
 	DecompressStringFile("out.th", "Out.txt");
-	//cout << CompressBinaryFile("in.txt", "out.b");
-	//DecompressBinaryFile("out.b", "out.txt");
-	//string str = ReadFileBinary("18120562_18120576.exe");
-	//cout << str;
 	system("pause");
 	return 0;
 }
